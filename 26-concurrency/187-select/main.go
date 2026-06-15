3753f1b18e450527530602e8c4581e6bab9b7ed3
Mario Lazzari <mario.lazzari@gmail.com>
2026-06-15 09:11:59 +0200

    Done lesson 188

--- 26-concurrency/187-select/main.go
+++ 26-concurrency/187-select/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	start := time.Now()
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		time.Sleep(time.Second)
+		ch1 <- "Ciao"
+	}()
+
+	go func() {
+		time.Sleep(time.Second)
+		ch2 <- "Hello"
+	}()
+
+	time.Sleep(time.Second)
+
+	// wait from multiple channels
+	for range 2 {
+		select {
+		case msg1 := <-ch1:
+			fmt.Println(msg1)
+		case msg2 := <-ch2:
+			fmt.Println(msg2)
+		default:
+
+			fmt.Println("No activity")
+		}
+	}
+
+	fmt.Printf("Exec time: %v\n", time.Since(start))
 }
